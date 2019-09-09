a6ad64df0f281a1128ebcc4bf1cb75a6ab3d754f
kewynakshlley <kewynakshlley@gmail.com>
2019-09-09 00:44:33 -0300

    adding angular features

--- src/main/java/com/ufpb/geekspace/GeekspaceApplication.java
+++ src/main/java/com/ufpb/geekspace/GeekspaceApplication.java
@@ -1,10 +1,24 @@
 package com.ufpb.geekspace;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GeekspaceApplication {
+	@RequestMapping("/resource")
+	public Map<String, Object> home() {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("id", UUID.randomUUID().toString());
+		model.put("content", "Hello World");
+		return model;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GeekspaceApplication.class, args);
