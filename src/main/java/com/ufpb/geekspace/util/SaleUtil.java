aa190024558cca478e1df7b6081253cb8562c194
kewynakshlley <kewynakshlley@gmail.com>
2019-09-21 03:21:21 -0300

    util files

---
+++ src/main/java/com/ufpb/geekspace/util/SaleUtil.java
@@ -0,0 +1,7 @@
+package com.ufpb.geekspace.util;
+
+public class SaleUtil {
+	public static final String SALE_NOT_FOUND = "The sale with this id do not exists.";
+	public static final String OUT_OF_STOCK = "STOCK NOT ENOUGH.";
+
+}
---
+++ src/main/java/com/ufpb/geekspace/util/UserUtil.java
@@ -0,0 +1,7 @@
+package com.ufpb.geekspace.util;
+
+public class UserUtil {
+	public static final String USER_NOT_FOUND = "The client doesn't exists";
+	public static final String USER_ALREADY_EXISTS = "An user with this e-email already exists.";
+	
+}
